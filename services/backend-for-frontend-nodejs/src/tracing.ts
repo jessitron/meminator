274fcc025917d842fcf82f9ca24318cd44e29a5f
Jessica Kerr <jessitron@gmail.com>
2024-06-19 14:27:43 -0500

    Well it has the right service name now
    and the right logging framework, but it still isn't working :cry:

--- services/backend-for-frontend-nodejs/src/tracing.ts
+++ services/backend-for-frontend-nodejs/src/tracing.ts
@@ -29,12 +29,19 @@ import {
     LoggerProvider,
     BatchLogRecordProcessor,
 } from "@opentelemetry/sdk-logs";
+import { Resource } from "@opentelemetry/resources";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 
 console.log("Initializing OpenTelemetry Logging...");
 
 // gotta make a provider
-const loggerProvider = new LoggerProvider();
+const loggerProvider = new LoggerProvider({
+    resource: new Resource({
+        // in order to give it a service name.
+        [SEMRESATTRS_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
+    }),
+});
 
 // tell it where to put them
 loggerProvider.addLogRecordProcessor(
@@ -56,4 +63,15 @@ logger.emit({
     severityText: 'JESS WAS HERE',
     body: 'this is a log record body',
     attributes: { 'stuff': 'and things' },
+});
+
+
+import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+
+registerInstrumentations({
+    instrumentations: [
+        new PinoInstrumentation({
+        }),
+    ],
 });

5434ffd0211a94dfc458cd6a17930bc77436d37f
Jessica Kerr <jessitron@gmail.com>
2024-06-24 19:10:55 -0500

    Consider the pixel density of the image

--- services/meminator-nodejs/src/applyTextWithImagemagick.ts
+++ services/meminator-nodejs/src/applyTextWithImagemagick.ts
@@ -21,6 +21,9 @@ export async function applyTextWithImagemagick(phrase: string, inputImagePath: s
         "app.meminate.maxWidthPx": IMAGE_MAX_WIDTH_PX,
     });
 
+    // don't wait for this. Just make a judgement
+    reportPredictedWidth(inputImagePath);
+
     const pointsize = 48;
     const font = 'Angkor-Regular';
     const args = [inputImagePath,
@@ -43,18 +46,31 @@ export async function applyTextWithImagemagick(phrase: string, inputImagePath: s
 }
 
 async function checkWhetherTextFits(pointsize: number, font: string, text: string, imageFilename: string) {
-    return inSpanAsync('measure text width', { attributes: { "text.pointsize": pointsize, "text.font": font, "text.content": text, "text.length": text.length } }, async (span) => {
-        const textWidth = await measureTextWidth(pointsize, font, text);
-        const imageWidth = await measureImageWidth(imageFilename);
+    return inSpanAsync('check text width', { attributes: { "text.pointsize": pointsize, "text.font": font, "text.content": text, "text.length": text.length } }, async (span) => {
+        const { width: imageWidth, density: imageDensity } = await measureImageWidthAndDensity(imageFilename);
+        span.setAttributes({ 'image.width': imageWidth, 'image.density': imageDensity });
+        const textWidth = await measureTextWidth(pointsize, font, imageDensity, text,);
         if (textWidth > imageWidth) {
-            logger.log('warn', `Text width is greater than image width: ${textWidth} > ${imageWidth}`, { "text.width": textWidth, "image.width": imageWidth, "text.content": text });
+            logger.log('warn', `Text width is greater than image width: ${textWidth} > ${imageWidth}`, {
+                "text.width": textWidth,
+                "image.width": imageWidth,
+                "text.content": text,
+                "image.density": imageDensity,
+                "text.length": text.length
+            });
         }
         span.setAttributes({ 'text.width': textWidth, "image.width": imageWidth, "text.doesItFit": textWidth <= imageWidth });
     });
 }
 
-async function measureTextWidth(pointsize: number, font: string, text: string): Promise<number> {
-    const result = await spawnProcess('convert', ['-pointsize', `${pointsize}`, '-font', `${font}`, '-format', '%w', `caption:${text}`, 'info:'])
+async function measureTextWidth(pointsize: number, font: string, imageDensity: number, text: string): Promise<number> {
+    const result = await spawnProcess('convert', [
+        '-pointsize', `${pointsize}`,
+        '-font', `${font}`,
+        '-format', '%w',
+        '-density', `${imageDensity}`,
+        `caption:${text}`,
+        'info:'])
     // convert stdout to int
     const width = parseInt(result.stdout);
     if (Number.isNaN(width)) {
@@ -63,14 +79,44 @@ async function measureTextWidth(pointsize: number, font: string, text: string): 
     return width;
 }
 
-async function measureImageWidth(filepath: string) {
-    const result = await spawnProcess('identify', ['-format', '%w', filepath]).then((result) => {
-        logger.log('debug', `Identify on output file: ${result.stdout}`, { "identify.filepath": filepath, "identify.width": result.stdout, "identify.error": result.stderr })
-        return result
-    })
-    const width = parseInt(result.stdout);
-    if (Number.isNaN(width)) {
-        throw new Error(`Could not parse width from ImageMagick output: ${result.stdout}`);
+async function measureImageWidthAndDensity(filepath: string) {
+    return await spawnProcess('identify', ['-format', '%w %x', filepath]).then((result) => {
+        const [width, density] = result.stdout.split(' ').map((s) => parseInt(s));
+        logger.log('debug', `Identify on output file: ${result.stdout}`, {
+            "identify.filepath": filepath,
+            "identify.width": width,
+            "identify.density": density,
+            "identify.error": result.stderr
+        })
+        return { width, density }
+    });
+}
+
+async function reportPredictedWidth(imageFilename: string) {
+    return inSpanAsync('predict image width', { attributes: { "image.filename": imageFilename } }, async (span) => {
+        const width = await predictImageWidth(imageFilename);
+        span.setAttribute('image.predictedWidth', width);
+    });
+}
+
+async function predictImageWidth(imageFilename: string) {
+    const result = await spawnProcess('identify', ['-format', '%wx%h', imageFilename]);
+    if (result.code !== 0) {
+        throw new Error(`Could not get image dimensions from ImageMagick: ${result.stderr}`);
     }
-    return width;
+    const [width, height] = result.stdout.split('x').map((s) => parseInt(s));
+    // we are going to resize the  image to IMAGE_MAX_WIDTH_PX x IMAGE_MAX_HEIGHT_PX
+    const ratioForHeightLimitation = Math.max(IMAGE_MAX_HEIGHT_PX / height, 1);
+    const widthLimitedByHeight = width * ratioForHeightLimitation;
+    const finalWidth = Math.min(width, IMAGE_MAX_WIDTH_PX, widthLimitedByHeight)
+    trace.getActiveSpan()?.setAttributes({
+        "image.width": width,
+        "image.height": height,
+        "image.ratioForHeightLimitation": ratioForHeightLimitation,
+        "image.widthLimitedByHeight": widthLimitedByHeight,
+        "image.maxWidth": IMAGE_MAX_WIDTH_PX,
+        "image.maxHeight": IMAGE_MAX_HEIGHT_PX,
+        "image.finalWidth": finalWidth,
+    });
+    return finalWidth;
 }

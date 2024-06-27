e9f258b4846c7c6aee948a86f1dd64b4ce00164d
Jessica Kerr <jessitron@gmail.com>
2024-06-26 20:33:24 -0500

    I need to know when loadgen is sending stuff

--- services/loadgen/loadgen.sh
+++ services/loadgen/loadgen.sh
@@ -10,6 +10,19 @@ echo "Min sleep: $MIN_SLEEP"
 echo "Max sleep: $MAX_SLEEP"
 echo "URL to hit: $LOAD_URL"
 
+function tell_hny_about_sleep {
+    duration=$1
+    body=$(echo '{
+    "name": "sleep between requests",
+    "duration_ms":' $duration '
+  }')
+    curl -i -X POST \
+  'https://api.honeycomb.io/1/events/loadgen' \
+  -H 'Content-Type: application/json' \
+  -H "X-Honeycomb-Team: $HONEYCOMB_API_KEY " \
+  -d $body
+}
+
 while true; do
     # Call the endpoint and print the HTTP status code
     curl -o /dev/null -X POST -w "%{http_code}\n" $LOAD_URL  &
@@ -17,5 +30,6 @@ while true; do
     # Sleep for a random time between 1 and 2 seconds
     sleep_time=$(awk -v min=$MIN_SLEEP -v max=$MAX_SLEEP 'BEGIN{srand(); print min+rand()*(max-min)}')
     echo "Sleeping for $sleep_time"
+    tell_hny_about_sleep $sleep_time
     sleep $sleep_time
 done

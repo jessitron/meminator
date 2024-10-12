461f2319901152f35c37bb284a293098bbcfd5f4
Jessica Kerr <jessitron@gmail.com>
2024-10-11 22:08:12 -0500

    I am terrible at this

--- scripts/create-marker.sh
+++ scripts/create-marker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-API_KEY=${HONEYCOMB_API_KEY_PROD_MARKERS:-$HONEYCOMB_API_KEY}
-
 ref=$(git rev-parse HEAD | cut -c1-10)
 if [[ -z "$VERSION_NUMBER" ]]; then
     message="ref:$ref"
@@ -12,16 +10,16 @@ body=$(echo '{"message":"'$message'", "type":"deploy"}')
 
 if [[ -z "$1" ]]; then
   dataset="__all__"
-else if [[ "$1" == "web" ]]; then
+elif [[ "$1" == "web" ]]; then
   dataset="web"
-else if [[ "$1" == "loadgen" ]]; then
+elif [[ "$1" == "loadgen" ]]; then
   dataset="meminator-loadgen"
 else
-  dataset="$1-nodejs" # this is the OTEL_SERVICE_NAME from docker-compose.yml or k8s.yaml for the service of name $1. We happen to have a convention, it won't work for 'web'
+  dataset="$1-nodejs" 
 fi
 
 echo "dataset: $dataset"
 
 curl https://api.honeycomb.io/1/markers/${dataset} -X POST  \
-    -H "X-Honeycomb-Team: ${API_KEY}"  \
+    -H "X-Honeycomb-Team: ${HONEYCOMB_API_KEY}"  \
     -d "$body"
